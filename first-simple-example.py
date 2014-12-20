513bd25ca30ebdbe5086c003ecd9a79d1b4d6fd3
tpphu <vietean@gmail.com>
2014-12-20 14:09:31 +0700

    Update the explain

--- first-simple-example.py
+++ first-simple-example.py
@@ -4,7 +4,7 @@ from django.conf import settings
 settings.configure(
     DEBUG=True,
     SECRET_KEY='thisisthesecretkey',
-    ROOT_URLCONF=__name__,
+    ROOT_URLCONF=__name__,#Cai nay nghia la sao ta?
     MIDDLEWARE_CLASSES=(
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
@@ -14,14 +14,20 @@ settings.configure(
 from django.conf.urls import url
 from django.http import HttpResponse
 
-
+# Cho nay thi minh hieu, giong nhu micro framework thui
 def index(request):
     return HttpResponse('Hello World')
 
-
+# Define lai router, co suc manh giong Zend khong?
 urlpatterns = (
     url(r'^$', index),
 )
+#
+#--------------------------------------------------------------------------------
+# 1. Mo ra port va listen tren port do
+# 2. Va giong nhu mot web server lun
+#--------------------------------------------------------------------------------
+#
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
