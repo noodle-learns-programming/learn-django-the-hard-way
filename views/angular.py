4a8b0eaa13e5bc31bfc9a1dc6b6c5fff80dede91
tpphu <vietean@gmail.com>
2015-01-04 13:08:19 +0700

    Add all files

--- views/angular.py
+++ views/angular.py
@@ -1,6 +1,54 @@
+from django import forms
+import hashlib
 from django.http import HttpResponse
+from PIL import Image, ImageDraw
+from io import BytesIO
+from django.views.decorators.http import etag
+
+class ImageForm(forms.Form):
+    """Form to validate requested placeholder image."""
+    height = forms.IntegerField(min_value=1, max_value=2000)
+    width = forms.IntegerField(min_value=1, max_value=2000)
+
+    def generate(self, image_format='PNG'):
+        """Generate an image of the given type and return as raw bytes."""
+        height = self.cleaned_data['height']
+        width = self.cleaned_data['width']
+        image = Image.new('RGB', (width, height))
+        draw = ImageDraw.Draw(image)
+        text = '{} X {}'.format(width, height)
+        textwidth, textheight = draw.textsize(text)
+        if textwidth < width and textheight < height:
+            texttop = (height - textheight) // 2
+            textleft = (width - textwidth) // 2
+            draw.text((textleft, texttop), text, fill=(255, 255, 255))
+        content = BytesIO()
+        image.save(content, image_format)
+        content.seek(0)
+        return content
+
+def generate_etag(request, width, height):
+    content = 'Placeholder: {0} x {1}'.format(width, height)
+    return hashlib.sha1(content.encode('utf-8')).hexdigest()
 
 #Thu key moi
 def add(req, n1, n2):
     content = 'Add: {0} + {1}'.format(n1, n2)
     return HttpResponse(content)
+
+@etag(generate_etag)
+def placeholder(req, width, height):
+    from pprint import pprint
+    form = ImageForm({'height': height, 'width': width})
+    pprint(vars(form))
+    if form.is_valid():
+        height = form.cleaned_data['height']
+        width = form.cleaned_data['width']
+        # TODO: Generate image of requested size
+        image = form.generate()
+        response = HttpResponse(image, content_type='image/png')
+        # response['Cache-Control'] = 'private, no-cache, no-store'
+        response['Max-Age'] = 86400
+        return response
+    else:
+        return HttpResponseBadRequest('Invalid Image Request')
--- views/urls.py
+++ views/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
+	url(r'^placeholder/(?P<width>\d+)x(?P<height>\d+)', 'views.angular.placeholder'),
     url(r'^add/(?P<n1>[0-9]+)/(?P<n2>[0-9]+)/$', 'views.angular.add')
 )
